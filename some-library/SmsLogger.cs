bb0e41fdccdca821533c3299dc6a6c79a1ff5afb
Justin Young <whyjustin@yahoo.com>
2017-09-20 12:16:13 -0700

    Some sms logger library

---
+++ some-library/SmsLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Twilio.TwiML;
+
+using static Newtonsoft.Json.JsonConvert;
+
+namespace some_library
+{
+    public class SmsLogger
+    {
+        public static void Log() {
+            var messagingResponse = new MessagingResponse();
+            messagingResponse.Message("Mr. Watson--come here--I want to see you.");
+            var serialized = SerializeObject(messagingResponse);
+
+            Console.Log(serialized);
+        }
+    }
+}
